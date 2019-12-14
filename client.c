b9b0cfa5cc747fe9156e4ba3614ff9ffa8f88293
FMiranda97 <36994381+FMiranda97@users.noreply.github.com>
2019-12-14 15:57:48 +0000

    all project

---
+++ client.c
@@ -0,0 +1,49 @@
+/**********************************************************************
+ * CLIENTE liga ao servidor (definido em argv[1]) no porto especificado  
+ * (em argv[2]), escrevendo a palavra predefinida (em argv[3]).
+ * USO: >cliente <enderecoServidor>  <porto>  <Palavra>
+ **********************************************************************/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+void erro(char *msg);
+
+int main(int argc, char *argv[]) {
+    char endServer[100];
+    int fd;
+    struct sockaddr_in addr;
+    struct hostent *hostPtr;
+
+    if (argc != 4) {
+        printf("cliente <host> <port> <string>\n");
+        exit(-1);
+    }
+
+    strcpy(endServer, argv[1]);
+    if ((hostPtr = gethostbyname(endServer)) == 0)
+        erro("Nao consegui obter endereço");
+
+    bzero((void *) &addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = ((struct in_addr *)(hostPtr->h_addr))->s_addr;
+    addr.sin_port = htons((short) atoi(argv[2]));
+
+    if((fd = socket(AF_INET,SOCK_STREAM,0)) == -1)
+        erro("socket");
+    if( connect(fd,(struct sockaddr *)&addr,sizeof (addr)) < 0)
+        erro("Connect");
+    write(fd, argv[3], 1 + strlen(argv[3]));
+    close(fd);
+    exit(0);
+}
+
+void erro(char *msg) {
+    printf("Erro: %s\n", msg);
+    exit(-1);
+}
---
+++ cmake-build-debug/CMakeFiles/IRCproj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IRCproj.dir/main.c.o"
+  "IRCproj"
+  "IRCproj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IRCproj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
