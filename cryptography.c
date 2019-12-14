07602f3af36e32db48649ef3d1cd9df219440ecd
Fmiranda97 <brugesmiranda@gmail.com>
2019-12-14 23:47:38 +0000

    Working example of how to use cryptography with sodium

--- cryptography.c
+++ cryptography.c
@@ -6,28 +6,42 @@
 #include <string.h>
 
 int main() {
+    //working example
+    //Must assume all parties have the key, so we share it through a file PSK
+    //Must send over the encrypted content, the nonce and the encrypted content size
+
     unsigned char key[crypto_secretbox_KEYBYTES];
+    FILE *fp;
+
+
     //init library
     if (sodium_init() < 0) {
         printf("Sodium library failed, aborting\n");
         exit(-1);
     }
 
-    //generate 256 bit key (32*8 = 256)
-    crypto_secretbox_keygen(key);
-    FILE *fp = fopen("PSK", "wb");
-    fwrite(key, 1, 32, fp);
-    fclose(fp);
+    fp = fopen("PSK", "rb");
+    if (!fp) {
+        printf("generating key\n");
+        //generate 256 bit key (32*8 = 256)
+        crypto_secretbox_keygen(key);
+        fp = fopen("PSK", "wb");
+        fwrite(key, 1, crypto_secretbox_KEYBYTES, fp);
+        fclose(fp);
+    } else {
+        printf("getting key\n");
+        fread(key, 1, crypto_secretbox_KEYBYTES, fp);
+        fclose(fp);
+    }
 
     unsigned char nonce[crypto_secretbox_NONCEBYTES];
     randombytes_buf(nonce, sizeof(nonce));
-    //memset(nonce, 0, sizeof(nonce));
     //set a string
     unsigned char string[1000] = "Isto e um belo teste de encriptacao";
     unsigned char encrypted[1000];
 
     printf("original:\n%s\n", string);
-    int a = strlen(string);
+    int a = (int) strlen(string);
     //encrypt
     crypto_secretbox_easy(encrypted, string, a, nonce, key);
     printf("encriptada:\n%s\n", encrypted);
